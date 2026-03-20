00bd1d1b1dd7a9cba389b67feab5d1908807f1ae
gussi121 <gussi121@gmail.com>
2026-03-20 10:46:27 +0200

    use fproc_open in command instead of exec

--- src/Command.php
+++ src/Command.php
@@ -1,4 +1,6 @@
-<?php namespace thiagoalessio\TesseractOCR;
+<?php
+
+namespace thiagoalessio\TesseractOCR;
 
 class Command
 {
@@ -13,13 +15,16 @@ class Command
 	public $imageSize;
 	private $outputFile;
 
-	public function __construct($image=null, $outputFile=null)
+	public function __construct($image = null, $outputFile = null)
 	{
 		$this->image = $image;
 		$this->outputFile = $outputFile;
 	}
 
-	public function build() { return "$this"; }
+	public function build()
+	{
+		return "$this";
+	}
 
 	public function __toString()
 	{
@@ -39,12 +44,12 @@ class Command
 		return join(' ', $cmd);
 	}
 
-	public function getOutputFile($withExt=true)
+	public function getOutputFile($withExt = true)
 	{
 		if (!$this->outputFile)
 			$this->outputFile = $this->getTempDir()
-				.DIRECTORY_SEPARATOR
-				.basename(tempnam($this->getTempDir(), 'ocr'));
+				. DIRECTORY_SEPARATOR
+				. basename(tempnam($this->getTempDir(), 'ocr'));
 		if (!$withExt) return $this->outputFile;
 
 		$hasCustomExt = array('hocr', 'tsv', 'pdf');
@@ -59,14 +64,14 @@ class Command
 
 	public function getTesseractVersion()
 	{
-		exec(self::escape($this->executable).' --version 2>&1', $output);
+		$this->exec(self::escape($this->executable) . ' --version 2>&1', $output);
 		$outputParts = explode(' ', $output[0]);
 		return $outputParts[1];
 	}
 
 	public function getAvailableLanguages()
 	{
-		exec(self::escape($this->executable) . ' --list-langs 2>&1', $output);
+		$this->exec(self::escape($this->executable) . ' --list-langs 2>&1', $output);
 		array_shift($output);
 		sort($output);
 		return $output;
@@ -75,6 +80,43 @@ class Command
 	public static function escape($str)
 	{
 		$charlist = strtoupper(substr(PHP_OS, 0, 3)) == 'WIN' ? '$"`' : '$"\\`';
-		return '"'.addcslashes($str, $charlist).'"';
+		return '"' . addcslashes($str, $charlist) . '"';
+	}
+
+	private function exec($command, &$output = null, &$return_val = null)
+	{
+		try {
+
+
+			$descriptorspec = [
+				0 => ["pipe", "r"], // stdin is a pipe that the child will read from
+				1 => ["pipe", "w"], // stdout is a pipe that the child will write to
+				2 => ["pipe", "w"]  // stderr is a pipe that the child will write to
+			];
+			$process = proc_open($command, $descriptorspec, $pipes);
+			if (is_resource($process)) {
+				$output = array();
+				while (($line = fgets($pipes[1])) !== false) { //go through empty lines too
+					// array_push($output,$line);
+					$output[] = $line;
+				}
+
+				if (!empty($output)) {
+					return rtrim(end($output));
+				} else {
+					return "";
+				}
+			} else {
+				return false;
+			}
+		} catch (\Exception $e) { //we will never be here as exec donesn't throw execptions even upon failure
+
+			return false;
+		} finally {
+			fclose($pipes[0]);
+			fclose($pipes[1]);
+			fclose($pipes[2]);
+			$return_val = proc_close($process);
+		}
 	}
 }
